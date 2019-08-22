b8579a52dd5305c4974eecc204ce7bdabb57a911
James Brown <44003186+JimmyRBob@users.noreply.github.com>
2019-08-22 12:30:08 +0100

    Add an initial arduino test script
    Add an initial Arduino test script to investigate the capabilities of Arduinos as I2C servo controllers.

---
+++ SerialServo.ino
@@ -0,0 +1,146 @@
+/***************************************************
+  This is an example for our Adafruit 16-channel PWM & Servo driver
+  Servo test - this will drive 16 servos, one after the other
+
+  Pick one up today in the adafruit shop!
+  ------> http://www.adafruit.com/products/815
+
+  These displays use I2C to communicate, 2 pins are required to
+  interface. For Arduino UNOs, thats SCL -> Analog 5, SDA -> Analog 4
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+// called this way, it uses the default address 0x40
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+// you can also call it with a different address you want
+//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
+
+// Depending on your servo make, the pulse width min and max may vary, you
+// want these to be as small/large as possible without hitting the hard stop
+// for max range. You'll have to tweak them as necessary to match the servos you
+// have!
+#define SERVOMIN  100 // this is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  525 // this is the 'maximum' pulse length count (out of 4096)
+
+// our servo # counter
+uint8_t servonum = 0;
+
+void setup() {
+  Serial.setTimeout(10);
+  Serial.begin(9600);
+  Serial.println("16 channel Servo test!");
+
+  pwm.begin();
+
+  pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
+
+  yield();
+}
+
+// you can use this function if you'd like to set the pulse length in seconds
+// e.g. setServoPulse(0, 0.001) is a ~1 millisecond pulse width. its not precise!
+void setServoPulse(uint8_t n, double pulse) {
+  double pulselength;
+
+  pulselength = 1000000;   // 1,000,000 us per second
+  pulselength /= 60;   // 60 Hz
+  Serial.print(pulselength); Serial.println(" us per period");
+  pulselength /= 4096;  // 12 bits of resolution
+  Serial.print(pulselength); Serial.println(" us per bit");
+  pulse *= 1000;
+  pulse /= pulselength;
+  Serial.println(pulse);
+  pwm.setPWM(n, 0, pulse);
+}
+
+void loop() {
+  // Drive each servo one at a time
+
+
+  if (Serial.available() > 0)
+  {
+    String Data = Serial.readStringUntil('#');
+
+    Serial.println("Recieved: " + Data);
+
+
+    String RemainingData = Data;
+    //ExampleData:
+
+    //0090.0-0190.0-0290.0-#
+
+    bool Finished = false;
+
+    if (RemainingData.length() < 3)
+    {
+      Finished = true;
+    }
+
+    while (Finished == false)
+    {
+      int ServoID = RemainingData.substring(0, 2).toInt();
+      Serial.println(ServoID);
+
+      RemainingData = RemainingData.substring(2);
+
+      int index = RemainingData.indexOf('-');
+
+      float ServoPosition = (RemainingData.substring(0, index - 1)).toFloat();
+
+      Serial.println(ServoPosition);
+
+
+      RemainingData = RemainingData.substring(index + 1);
+
+
+      float valueToGoTo = ((SERVOMAX - SERVOMIN) * (ServoPosition / 180)) + SERVOMIN;
+
+
+      if (valueToGoTo < SERVOMIN)
+      {
+        valueToGoTo = SERVOMIN;
+      }
+      else if (valueToGoTo > SERVOMAX)
+      {
+        valueToGoTo = SERVOMAX;
+      }
+
+
+
+      Serial.println(valueToGoTo);
+
+      pwm.setPWM(ServoID, 0, valueToGoTo);
+
+      if (RemainingData.length() < 3)
+      {
+        Finished = true;
+      }
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
